983f090340b88c784a18ef349bd8b1162a174f07
Aden Robertson <alrob2224@gmail.com>
2024-05-31 11:53:15 -0400

    basic basic movement

---
+++ Assets/Scripts/BasicControl.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Windows;
+
+public class BasicControl : MonoBehaviour
+{
+    private CharacterController cCtrl;
+    private Rigidbody rb;
+
+    public float forwardMaxSpeed = 1f;
+    public float turnMaxSpeed = 1f;
+
+    void Awake()
+    {
+        cCtrl = GetComponent<CharacterController>();
+        rb = GetComponent<Rigidbody>();
+    }
+
+    void Update()
+    {
+        float inputForward = 0f;
+        float inputTurn = 0f;
+
+        if (cCtrl.enabled)
+        {
+            inputForward = cCtrl.Forward;
+            inputTurn = cCtrl.Turn;
+        }
+
+        if (inputForward < 0f)
+        {
+            inputTurn = -inputTurn;
+        }
+
+        rb.MovePosition(rb.position + this.transform.forward * inputForward * Time.deltaTime * forwardMaxSpeed);
+        rb.MoveRotation(rb.rotation * Quaternion.AngleAxis(inputTurn * Time.deltaTime * turnMaxSpeed, Vector3.up));
+    }
+}
