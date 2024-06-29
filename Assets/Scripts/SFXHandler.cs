d85ca55de15375e080a911e44b4957e0b5449076
kruti <kmaheshwari32@gatech.edu>
2024-06-29 16:13:15 -0400

    added opening music and rowing sfx to alpha scene

---
+++ Assets/Scripts/SFXHandler.cs
@@ -0,0 +1,59 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//this class handles two secret continuous audio loops in the background
+//that sound like constant paddling and constantly being chased by the shark
+//these audio loops are muted and unmuted using this script, according to
+//when the player performs certain actions
+public class SFXHandler : MonoBehaviour
+{   
+    //public GameObject Player; //see if the player has moved. if they have, keep playing the rowing sound. if no movement, mute the rowing sound
+    public KayakMovement kayak;
+    //Vector3 playerPrevPos;
+    public AudioSource rowing;
+    //public AudioSource shark;
+    public bool isRowing; //when the player is rowing, the rowing sound must play
+    //public bool isSharkStalked; //when a shark is actively pursing the player, the shark sound must play
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        isRowing = false;
+        //isSharkStalked = false;
+       // playerPrevPos = Player.transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (kayak.currentStrokeState == StrokeState.Ready) {
+            rowing.mute = true;
+            isRowing = false;
+        } else {
+            rowing.mute = false;
+            isRowing = true;
+        }
+        /*if(playerPrevPos != Player.transform.position) {
+            isRowing = true;
+        } else {
+            isRowing = false;
+        }
+
+        if(isRowing) {
+            rowing.mute = false; //add fade out here
+        } else {
+            rowing.mute = true; //add fade in here
+        }
+
+        /*if(isSharkStalked) {
+            shark.mute = false; //add fade out here
+        } else {
+            shark.mute = true; //add fade in here
+        } */
+        
+       // playerPrevPos = Player.transform.position;
+    }
+
+
+}
