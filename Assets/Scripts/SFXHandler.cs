a0fab578ef9a526497bfbda975e8a6ccaeeb94d1
kruti <kmaheshwari32@gatech.edu>
2024-06-29 01:19:45 -0400

    added boat paddling sound effect to kruti_sfx1 scene

--- Assets/Scripts/SFXHandler.cs
+++ Assets/Scripts/SFXHandler.cs
@@ -2,12 +2,16 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-//this class is attached to an audio source object with the same transform as the player
-//allowing sfx to play from the player's position
-//the sfx are chosen using a state machine
-//all sounds are from freesound.org, see readme for specific citations
+//this class handles two secret continuous audio loops in the background
+//that sound like constant paddling and constantly being chased by the shark
+//these audio loops are muted and unmuted using this script, according to
+//when the player performs certain actions
 public class SFXHandler : MonoBehaviour
-{
+{   
+    public GameObject Player; //see if the player has moved. if they have, keep playing the rowing sound. if no movement, mute the rowing sound
+    Vector3 playerPrevPos;
+    public AudioSource rowing;
+    public AudioSource shark;
     public bool isRowing; //when the player is rowing, the rowing sound must play
     public bool isSharkStalked; //when a shark is actively pursing the player, the shark sound must play
 
@@ -15,11 +19,31 @@ public class SFXHandler : MonoBehaviour
     void Start()
     {
         isRowing = false;
+        isSharkStalked = false;
+        playerPrevPos = Player.transform.position;
     }
 
     // Update is called once per frame
     void Update()
     {
+        if(playerPrevPos != Player.transform.position) {
+            isRowing = true;
+        } else {
+            isRowing = false;
+        }
+
+        if(isRowing) {
+            rowing.mute = false; //add fade out here
+        } else {
+            rowing.mute = true; //add fade in here
+        }
+
+        if(isSharkStalked) {
+            shark.mute = false; //add fade out here
+        } else {
+            shark.mute = true; //add fade in here
+        }
         
+        playerPrevPos = Player.transform.position;
     }
 }
